d17824b7761314840c5ddb078b1e751e4433cb4f
Zack Synnestvedt <58566643+ZackSynnestvedt@users.noreply.github.com>
2019-12-20 12:33:46 -0500

    Update example0.py
    hfjfjh

--- example0.py
+++ example0.py
@@ -18,7 +18,7 @@ def move(my_history, their_history, my_score, their_score):
     The first round between these two players is my_history[0] and their_history[0]
     The most recent round is my_history[-1] and their_history[-1]
     
-    Returns 'z' or 'b' for collude or betray.
+    Returns 'z' or 'c' for collude or betray.
     '''
     
     # This player always colludes.
